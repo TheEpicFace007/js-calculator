f82a135603768a483fcaf6188b64b8f495161d8b
Vini Dalvino <37189791+PissMan420@users.noreply.github.com>
2022-03-02 22:13:52 -0500

    Refactored app

--- app.js
+++ app.js
@@ -2,12 +2,12 @@
 const result = document.getElementById('result');
 const stats = document.getElementById('num-calc');
 
-stats.innerText = `${localStorage.numCalc ?? 0} equation computed 💻🧮`
+stats.innerText = `${localStorage.numCalc || 0} equation computed 💻🧮`
 
-document.querySelectorAll('.button').forEach(button => {
+document.querySelectorAll('.calc-btn').forEach(button => {
     button.addEventListener('click', function () {
-        const operators = [['add', '+'], ['substract', '-'], ['multiply', '*'],
-            ['divide', '/']];
+        const operators = [['add', '+'], ['substract', '-'], ['multiply', '*', '×'],
+            ['divide', '/', '÷']];
         const operator = operators.find(x => x[0] === button.id);
         result.style.color = 'black';
         if (button.id == 'clear') {
@@ -16,7 +16,12 @@ document.querySelectorAll('.button').forEach(button => {
 ;
 
             try {
-                result.value += `= ${eval(result.value)}`;
+                let logs = localStorage.getItem('logs');
+                logs = logs ? JSON.parse(logs) : [];
+                logs.push({ calculation: result.value, date: new Date() });
+                localStorage.setItem('logs', JSON.stringify(logs));
+
+                result.value = `${eval(result.value.replaceAll('×', '*').replaceAll('÷', '/'))}`;
 
                 let numCalc = localStorage.getItem('numCalc')
                 if (numCalc) {
@@ -27,15 +32,31 @@ document.querySelectorAll('.button').forEach(button => {
 
                 numCalc++;
                 localStorage.setItem('numCalc', numCalc);
+                stats.innerText = `${numCalc} equation computed 💻🧮`;
             } catch (e) {
                 if (e instanceof SyntaxError) {
                     result.value = 'Syntax Error';
                     result.style.color = 'red';
                 }
+                console.error(e);
             }
-            stats.innerText = `${numCalc} equation computed 💻🧮`;
         } else {
             result.value += button.innerText;
         }
     });
+});
+
+document.getElementById('save-history').addEventListener('click', function () {
+    // open a save as dialog
+    const logs = JSON.parse(localStorage.logs) ?? [];
+    let logsTxt = '';
+    logs.forEach(log => {
+        logsTxt += `${log.date.toLocaleString('en-ca')} - ${log.calculation}\n`;
+    });
+    const blob = new Blob([logsTxt], {type: 'text/plain'});
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'calculator-history.txt';
+    link.click();
 });
